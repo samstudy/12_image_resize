6b4e2af750b649950beeef8a52142711c670b2d9
Samat Karymshakov <bskordo@gmail.com>
2016-12-04 17:57:37 +0600

    changed a value define

--- image_resize.py
+++ image_resize.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', help='Mandatory parametr')
+    parser.add_argument('source_path', help='Mandatory parametr')
     parser.add_argument('--width', type=int)
     parser.add_argument('--height', type=int)
     parser.add_argument('--scale', type=float)
@@ -14,28 +14,27 @@ def get_args():
     return parser.parse_args()
 
 
-def new_size(width, height, scale):
+def get_new_size(width, height, scale):
+    initial_scale = get_old_scale()
     if scale is not None:
         return int(img.width * scale), int(img.height * scale)
     if width is None:
-        return (old_scale * height), height
+        return (initial_scale * height), height
     if height is None:
-        value = old_scale()
-        return width, int(width / value)
+        return width, int(width / initial_scale)
     elif width and height is not None:
-        value = old_scale()
-        if (width / height) != value:
+        if (width / height) != initial_scale:
             print('Scale is not same as initial value')
         return width, height
        
             
-def old_scale():
+def get_old_scale():
     old_scale = (old_width)/(old_height)
     return int(old_scale)
 
 
 def resize_img(img, width=None, height=None, scale=None):
-    size = new_size(width, height, scale)
+    size = get_new_size(width, height, scale)
     return img.resize(size)
 
 
@@ -52,15 +51,15 @@ def output_for_updated_img(output_path, path_to_image, img):
 
 if __name__ == '__main__':
     params = get_args()
-    if not os.path.exists(params.input):
+    if not os.path.exists(params.source_path):
         print('There is none image for modify')
         exit()
     if params.scale is not None and (params.width or params.height is not None):
         print('You should specify only scale or width and height')
         exit()
-    img = Image.open(params.input)
+    img = Image.open(params.source_path)
     old_width, old_height = img.size
     updated_image = resize_img(img, width=params.width, height=params.height, scale=params.scale)
-    path_to_result = output_for_updated_img(params.output, params.input, updated_image)
+    path_to_result = output_for_updated_img(params.output, params.source_path, updated_image)
     updated_image.save(path_to_result)
     
